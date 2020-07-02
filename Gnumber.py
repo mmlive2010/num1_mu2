04d3534f4127a89700d063b3aed2d0d2ec7e6769
MosquitoX <ali.emara62@gmail.com>
2020-07-02 13:21:15 +0200

    Create Gnumber.py

---
+++ Gnumber.py
@@ -0,0 +1,14 @@
+#hello_My friend this is so easy script to
+#first i will define the numbers 
+num1 = input('Input Your First Number : ')
+num1 = int(num1)
+num2 = input('Input Your Second Number : ')
+num2 = int(num2)
+
+if num1 > num2 : 
+  print("The First Number Is Greater Than Second Number ")
+if num2 > num1 :
+  print("The Second Number Is Greater Than First Number ")
+else :
+  print("Both Number Are Equal")
+print("THANKS FOR USING MY SCRIPT BY ALI EMARA ")
